f1b0ed6b020841cdfe88ad5b29b5dae70d56e5f7
Philip Gillespie <philipgillespie91@gmail.com>
2025-05-20 16:00:30 +0100

    Add hardmode and precog to improve keyboard use

---
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+return {
+	"m4xshen/hardtime.nvim",
+	event = "VeryLazy",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		max_count = 2,
+		disable_mouse = false,
+        restriction_mode="block",
+	},
+}
