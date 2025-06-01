1976a8924b84af8b5f06a40fec07e249150eb6fd
Philip Gillespie <philipgillespie91@gmail.com>
2025-06-01 22:07:15 +0100

    Update completions and lsp for Rust

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -19,26 +19,50 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
 			cmp.setup({
-				preselect = cmp.PreselectMode.None,
-				completion = {
-					completeopt = "menu,menuone,noinsert,noselect",
+				formatting = {
+					format = function(entry, vim_item)
+						-- Truncate overly long detail (e.g., function signatures)
+						local detail = entry.completion_item.detail
+						if detail and #detail > 40 then
+							vim_item.menu = detail:sub(1, 27) .. "..."
+						else
+							vim_item.menu = detail
+						end
+
+						return vim_item
+					end,
 				},
-				-- completion = { autocomplete = false },
+				-- preselect = cmp.PreselectMode.None,
+				preselect = cmp.PreselectMode.Item,
+				completion = {
+					-- completeopt = "menu,menuone,noinsert,noselect",
+					completeopt = "menu,menuone,noinsert",
+					autocomplete = false,
+				},
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered({ max_width = 30 }),
+					documentation = cmp.config.window.bordered({ max_width = 30 }),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<Tab>"] = function(_)
+						if cmp.visible() then
+							cmp.select_next_item()
+							-- else
+							-- 	cmp.complete()
+						end
+					end,
+					["<C-Space>"] = cmp.mapping(function()
+						cmp.complete()
+					end, { "i", "c" }),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					-- manually trigger the menu to appear
-					["<C-Space>"] = cmp.mapping.complete(),
+					-- ["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
 					["<C-n>"] = cmp.mapping(function(fallback)
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,14 @@
 -- lsp.lua
+-- Suppress rust-analyzer echo messages
+vim.lsp.handlers["window/showMessage"] = function(_, result, ctx) end
+vim.lsp.handlers["window/logMessage"] = function(_, _) end
 
 -- Disable virtual text and enable signs
 vim.diagnostic.config({
 	virtual_text = false,
 	signs = true,
 	severity_sort = true,
+	update_in_insert = false,
 })
 
 -- Define custom signs for diagnostics
@@ -26,7 +30,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "gopls", "clangd" },
+				ensure_installed = { "lua_ls", "pyright", "gopls", "clangd", "rust-analyzer" },
 			})
 		end,
 	},
@@ -35,13 +39,69 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			lspconfig.clangd.setup({ capabilities = { capabilities } })
-			lspconfig.lua_ls.setup({ capabilities = { capabilities } })
-			lspconfig.pyright.setup({ capabilities = { capabilities} })
+			capabilities.textDocument = capabilities.textDocument or {}
+			capabilities.textDocument.synchronization = {
+				dynamicRegistration = false,
+				willSave = false,
+				didSave = true,
+				willSaveWaitUntil = false,
+				change = 1, -- Full document sync instead of incremental DID NOT WORK
+			}
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
 			lspconfig.gopls.setup({
-				capabilities = { capabilities },
+				capabilities = capabilities,
 			})
-            lspconfig.texlab.setup({capabilities = capabilities})
+			lspconfig.texlab.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+
+				-- Prevent diagnostics while typing
+				handlers = {
+					["textDocument/publishDiagnostics"] = vim.lsp.with(
+						vim.lsp.handlers["textDocument/publishDiagnostics"],
+						{
+							virtual_text = false,
+							signs = true,
+							update_in_insert = false, -- Don't show diagnostics while typing
+						}
+					),
+				},
+
+				flags = {
+					debounce_text_changes = 1000,
+					allow_incremental_sync = false, -- Force full document sync
+				},
+				-- settings = {
+				-- 	["rust-analyzer"] = {
+				-- 		diagnostics = {
+				-- 			enable = true,
+				-- 			enableExperimental = false,
+				-- 			delay = 500, -- delay diagnostics 500ms after edit
+				-- 			disabled = { "unresolved-proc-macro" }, -- Disable noisy proc-macro warnings
+				-- 			warningsAsHint = { "clippy::all" }, -- Show clippy warnings as hints
+				-- 		},
+				-- 		cargo = {
+				-- 			allFeatures = true,
+				-- 			loadOutDirsFromCheck = true,
+				-- 		},
+				-- 		checkOnSave = {
+				-- 			command = "clippy", -- Optional: run clippy instead of check
+				-- 			extraArgs = { "--no-deps" }, -- Don't check dependencies
+				-- 		},
+				-- 		inlayHints = {
+				-- 			enable = false, -- reduce visual noise
+				-- 		},
+				-- 		semanticHighlighting = false, -- disable semantic tokens
+				-- 	},
+				-- },
+
+				-- -- Optional: disable formatting if you use null-ls
+				-- on_attach = function(client, bufnr)
+				-- 	client.server_capabilities.documentFormattingProvider = false
+				-- end,
+			})
 		end,
 	},
 	-- none-ls.lua
