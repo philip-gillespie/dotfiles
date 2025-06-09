ee850a4c800380296fa2cbc6362318eda2978c67
Philip Gillespie <philipgillespie91@gmail.com>
2025-06-09 16:11:56 +0100

    update keybinds to new style

--- nvim/.config/nvim/lua/plugins/keybinds.lua
+++ nvim/.config/nvim/lua/plugins/keybinds.lua
@@ -2,95 +2,72 @@
 local diagnostics_active = true
 
 local function toggle_diagnostics()
-	diagnostics_active = not diagnostics_active
-	if diagnostics_active then
-		vim.diagnostic.show()
-	else
-		vim.diagnostic.hide()
-	end
+    diagnostics_active = not diagnostics_active
+    if diagnostics_active then
+        vim.diagnostic.show()
+    else
+        vim.diagnostic.hide()
+    end
 end
 
 local function toggle_wrap()
-	local wo = vim.wo
-	wo.wrap = not wo.wrap
+    local wo = vim.wo
+    wo.wrap = not wo.wrap
 
-	if wo.wrap then
-		vim.opt.linebreak = true
-		vim.opt.breakindent = true
-	else
-		vim.opt.linebreak = false
-		vim.opt.breakindent = false
-	end
+    if wo.wrap then
+        vim.opt.linebreak = true
+        vim.opt.breakindent = true
+    else
+        vim.opt.linebreak = false
+        vim.opt.breakindent = false
+    end
 end
 
 -- keybinds.lua
 vim.g.mapleader = " " -- Set Leader Keybind
 -- Plugin Keybindings
 local keybinds = {
-	["<leader>"] = {
-		b = {
-			name = "Buffers",
-			n = { "<cmd>bn<cr>", "Next" },
-			p = { "<cmd>bp<cr>", "Previous" },
-			x = { "<cmd>bd<cr>", "Delete" },
-		},
-		e = {
-			name = "Diagnostic",
-			t = {
-				function()
-					toggle_diagnostics()
-				end,
-				"Toggle",
-			},
-			i = { "<cmd>lua vim.diagnostic.open_float(nil, {focus=false})<cr>", "Info" },
-			n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next" },
-			p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Previous" },
-		},
-		f = {
-			name = "File", -- typically using the plugin Telescope
-			f = { "<cmd>Telescope find_files<cr>", "Find File" },
-			g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-			b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-			h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-		},
-		g = {
-			name = "LSP",
-			-- f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format File" },
-			f = {
-				function()
-					vim.lsp.buf.format({ timeout = 2000 })
-				end,
-				"Format File",
-			},
-			r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename Symbol" },
-			d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Get Definition" },
-		},
-		t = {
-			"<cmd>Neotree toggle<cr>",
-			"File Tree",
-		},
-		w = {
-			toggle_wrap,
-			"Toggle Wrap",
-		},
-		["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment" },
-	},
-	["<leader>/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment", mode = "v" },
-	["K"] = {
-		"<cmd>lua vim.lsp.buf.hover()<cr>",
-	},
+    { "<leader>/",  "<Plug>(comment_toggle_linewise_current)",                    desc = "Comment" },
+    { "<leader>b",  group = "Buffers" },
+    { "<leader>bn", "<cmd>bn<cr>",                                                desc = "Next" },
+    { "<leader>bp", "<cmd>bp<cr>",                                                desc = "Previous" },
+    { "<leader>bx", "<cmd>bd<cr>",                                                desc = "Delete" },
+    { "<leader>e",  group = "Diagnostic" },
+    { "<leader>ei", "<cmd>lua vim.diagnostic.open_float(nil, {focus=false})<cr>", desc = "Info" },
+    { "<leader>en", "<cmd>lua vim.diagnostic.goto_next()<cr>",                    desc = "Next" },
+    { "<leader>ep", "<cmd>lua vim.diagnostic.goto_prev()<cr>",                    desc = "Previous" },
+    { "<leader>et", toggle_diagnostics,                                           desc = "Toggle" },
+    { "<leader>f",  group = "File" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",                                 desc = "Buffers" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",                              desc = "Find File" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",                               desc = "Live Grep" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",                               desc = "Help Tags" },
+    { "<leader>g",  group = "LSP" },
+    { "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>",                      desc = "Get Definition" },
+    {
+        "<leader>gf",
+        function()
+            vim.lsp.buf.format({ timeout = 2000 })
+        end,
+        desc = "Format File",
+    },
+    { "<leader>gr", "<cmd>lua vim.lsp.buf.rename()<CR>",      desc = "Rename Symbol" },
+    { "<leader>t",  "<cmd>Neotree toggle<cr>",                desc = "File Tree" },
+    { "<leader>w",  toggle_wrap,                              desc = "Toggle Wrap" },
+    { "K",          desc = "<cmd>lua vim.lsp.buf.hover()<cr>" },
+    { "<leader>/",  "<Plug>(comment_toggle_linewise_visual)", desc = "Comment",      mode = "v" },
 }
 
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	config = function()
-		local wk = require("which-key")
-		wk.setup({})
-		wk.register(keybinds)
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    config = function()
+        local wk = require("which-key")
+        wk.setup({})
+        wk.add(keybinds)
+    end,
 }
