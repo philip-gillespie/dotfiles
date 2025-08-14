3608c3b08a23719e6e651f9f3996b1587749e5eb
Philip Gillespie <philipgillespie91@gmail.com>
2025-08-14 15:48:44 +0100

    Monkeypatch to show graphs in remote wezterm

---
+++ ipython/.ipython/profile_default/startup/01-wezterm.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import io
+import subprocess
+import tempfile
+
+def wezterm_show():
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
+        tmp.write(buf.read())
+        tmp.flush()
+        subprocess.run(["imgcat", tmp.name])
+    plt.close()
+    return None
+
+plt.show = wezterm_show
