c7104e00ac63fd7b1fd7dd7e4729dac9bcee7e50
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-03-12 17:59:46 +0000

    Add Hardtime to improve keystrokes

---
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+-- hardtime.lua
+-- Encouraging better use of keystrokes by blocking multiple presses.
+return {
+	"m4xshen/hardtime.nvim",
+	lazy = false,
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {},
+}
