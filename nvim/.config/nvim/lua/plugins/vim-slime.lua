50c27b15e81c93204e17d4551a4aba2458d6a4c8
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-03-26 11:15:27 +0000

    Add keybinds to vim slime

--- nvim/.config/nvim/lua/plugins/vim-slime.lua
+++ nvim/.config/nvim/lua/plugins/vim-slime.lua
@@ -7,10 +7,11 @@ return {
 	init = function()
 		-- 1. Set target to tmux
 		vim.g.slime_target = "wezterm"
+        vim.g.slime_no_mappings = 1
 
 		-- 2. Ensure IPython handles standard Python indentation correctly
-		vim.g.slime_python_ipython = 1
-
+		-- vim.g.slime_python_ipython = 1
+		vim.g.slime_bracketed_paste = 1
 		-- 3. Define what a "cell" looks like for your workflow
 		vim.g.slime_cell_delimiter = "# %%"
 
@@ -20,7 +21,16 @@ return {
 	-- The 'config' function runs AFTER the plugin is loaded.
 	-- This is where we set up our custom keybindings.
 	config = function()
-		-- Map <leader>c to send the current # %% cell to IPython
-		-- vim.keymap.set("n", "<leader>c", "<Plug>SlimeSendCell", { desc = "Send cell to IPython" })
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>s", group = "Slime / Send" },
+			{ "<leader>sc", "<Plug>SlimeSendCell", desc = "Send Cell to IPython", remap = true },
+			{ "<leader>sp", "<Plug>SlimeParagraphSend", desc = "Send Paragraph", remap = true },
+			{ "<leader>sl", "<Plug>SlimeLineSend", desc = "Send Line", remap = true },
+			{ "<leader>sv", "<Plug>SlimeConfig", desc = "Configure Slime Pane", remap = true },
+
+			-- Visual mode bindings (note the mode = "x")
+			{ "<leader>s", "<Plug>SlimeRegionSend", desc = "Send Visual Selection", mode = "x", remap = true },
+		})
 	end,
 }
