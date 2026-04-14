03ae463c4d200c002bbad3a362cea76647ade5bf
Philip Gillespie <philipgillespie91@gmail.com>
2026-04-14 17:31:32 +0100

    Improve dynamic lua snippets

--- nvim/.config/nvim/lua/snippets/markdown.lua
+++ nvim/.config/nvim/lua/snippets/markdown.lua
@@ -8,27 +8,31 @@ local sn = ls.snippet_node
 local c = ls.choice_node
 local r = ls.restore_node
 
-function lua_repl_line(jump_index, depth, max_depth)
-	depth = depth or 1
-	max_depth = max_depth or 20
 
-	if depth > max_depth then
-		return t("")
+local function lua_repl_line(depth)
+	if depth > 50 then
+		return sn(nil, { t("") })
 	end
 
-	return c(jump_index, {
-		t(""), -- Option 1: Stop
-		sn(nil, { -- Option 2: New Line
-			t({ "", "> " }),
-			r(1, "in_" .. depth, i(1)),
-			t(" --> "),
-			r(2, "out_" .. depth, i(1)),
-			-- This dynamic node is jump index 3 within THIS line
-			d(3, function()
-				-- Inside the sn returned here, the choice_node
-				-- is the first/only item, so it MUST be jump index 1
-				return sn(nil, { lua_repl_line(1, depth + 1, max_depth) })
-			end, {}),
+	return sn(nil, {
+		c(1, {
+			t(""), -- Option 1: Stop (Default, prevents infinite loop)
+			sn(nil, { -- Option 2: Input + Output
+				t({ "", "" }),
+				r(1, "in_" .. depth, i(1)),
+				t(" --> "),
+				r(2, "out_" .. depth, i(1)),
+				d(3, function()
+					return lua_repl_line(depth + 1)
+				end, {}),
+			}),
+			sn(nil, { -- Option 3: Input only
+				t({ "", "" }),
+				r(1, "in_" .. depth, i(1)),
+				d(2, function()
+					return lua_repl_line(depth + 1)
+				end, {}),
+			}),
 		}),
 	})
 end
@@ -67,17 +71,23 @@ return {
 	}),
 	s("$$", { t({ "$$", "" }), i(1), t({ "", "$$" }) }),
 	s("matrix", { t("\\begin{pmatrix}"), i(1), t("\\end{pmatrix}") }),
-	s({ trig = "\\lua", dscr = "lua code block" }, {
-		t({ "```lua", "" }),
-		i(1),
-		t({ "", "```" }),
-	}),
-	s({ trig = "\\luarepl" }, {
-		t({ "```lua", "> " }),
-		i(1),
-		t({ " --> " }),
-		i(2),
-		lua_repl_line(3),
+	s({ trig = "\\lua" }, {
+		t("```lua"),
+		c(1, {
+			sn(nil, {
+				t({ "", "" }),
+				r(1, "in_0", i(1)),
+				t(" --> "),
+				r(2, "out_0", i(1)),
+			}),
+			sn(nil, {
+				t({ "", "" }),
+				r(1, "in_0", i(1)),
+			}),
+		}),
+		d(2, function()
+			return lua_repl_line(1)
+		end, {}),
 		t({ "", "```" }),
 	}),
 }
