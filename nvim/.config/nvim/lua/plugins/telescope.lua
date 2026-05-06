05fe213e00bf0ef54f4647d8e2abfb3cccae2282
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-05-06 08:27:25 +0100

    Add telescope file browsing

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,20 +3,32 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-        branch = "master",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
+		branch = "master",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
 		config = function()
-			-- This is your opts table
-			require("telescope").setup({
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local fb_actions = telescope.extensions.file_browser.actions
+
+			telescope.setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-
+						require("telescope.themes").get_dropdown({}),
+					},
+					file_browser = {
+						hijack_netrw = true,
+						hidden = true,
+						mappings = {
+							-- ["n"] dictates Normal mode overrides specifically for file_browser
+							["n"] = {
+								["h"] = fb_actions.goto_parent_dir,
+								["l"] = actions.select_default,
+							},
+						},
 					},
 				},
 			})
