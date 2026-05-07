84ab81b7d223890823c245a0e8a31580ad9324d4
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-05-07 17:54:57 +0100

    update aerial navigation

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -4,9 +4,16 @@ local function setup()
 		backends = { "treesitter" },
 		filter_kind = false,
 		close_on_select = true,
-		layout = { min_width = 20, default_direction = "float", max_width = 0.8 },
+		layout = { min_width = 20, default_direction = "float", max_width = 0.9 },
 		float = { relative = "editor" },
-		nav = { preview = true, max_width = 0.2 },
+		nav = {
+			preview = true,
+			max_width = 0.8,
+			keymaps = {
+				["q"] = "actions.close",
+				["e"] = "actions.jump",
+			},
+		},
 		manage_folds = false,
 		link_tree_to_folds = false,
 		link_folds_to_tree = false,
@@ -20,12 +27,11 @@ local function setup()
 	vim.keymap.set("n", "<leader> ", "<cmd>AerialNavToggle<CR>")
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = { "aerial", "aerial-nav" },
-		callback = function(args)
+		callback = function()
 			vim.schedule(function()
 				vim.wo.number = true
 				vim.wo.relativenumber = true
 			end)
-			vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = args.buf, silent = true })
 		end,
 	})
 end
