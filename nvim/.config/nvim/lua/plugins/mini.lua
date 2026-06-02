0d8466c71071e7f5e4dedfc4a4a599ad38b3d6fd
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-06-02 08:51:46 +0100

    Add mini config

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,15 +1,16 @@
 return {
 	{
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	{
 		"echasnovski/mini.surround",
 		version = false,
 		config = function()
 			require("mini.surround").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.ai",
+		version = "*",
+		config = function()
+			require("mini.ai").setup()
 		end,
 	},
 }
