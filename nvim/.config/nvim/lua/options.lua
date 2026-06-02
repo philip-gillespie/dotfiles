14400a4cc5ec03de9c4c18c7a7b7ac9e0bace6bc
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-06-02 08:29:45 +0100

    Add options for closing and opening floating terminal

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -10,10 +10,44 @@ vim.opt.clipboard = "unnamedplus"
 -- GUI
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.wrap = false -- display long lines as one long line
+
+-- Cursor
+vim.opt.guicursor =
+	"n-v-c-sm:block,i-ci-ve:ver25-blinkon500-blinkoff500,r-cr-o:hor20,t:ver25-blinkon500-blinkoff500-TermCursor"
+
 -- Line numbers
 vim.opt.number = true -- set numbered lines
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+
+local term_settings = vim.api.nvim_create_augroup("TerminalSettings", { clear = true })
+
+-- Set numbers ON when leaving Terminal mode (Normal mode)
+vim.api.nvim_create_autocmd("TermLeave", {
+	group = term_settings,
+	pattern = "term://*",
+	callback = function()
+		vim.opt_local.relativenumber = true
+	end,
+})
+
+-- Set numbers OFF when entering Terminal mode (Insert/Input mode)
+vim.api.nvim_create_autocmd("TermEnter", {
+	group = term_settings,
+	pattern = "term://*",
+	callback = function()
+		vim.opt_local.relativenumber = false
+	end,
+})
+
+-- Ensure new terminals start without numbers (as they start in Terminal mode)
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = term_settings,
+	callback = function()
+		vim.opt_local.relativenumber = false
+	end,
+})
+
 -- Do not expand tabs into spaces
 vim.o.expandtab = true
 -- Tab width to 4 spaces
@@ -69,10 +103,24 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		"tsplayground",
 		"",
 	},
-	callback = function()
-		vim.cmd([[
-      nnoremap <silent> <buffer> q :close<CR>
-      set nobuflisted
-    ]])
+	callback = function(event)
+		vim.keymap.set("n", "q", "<cmd>close<cr>", {
+			buffer = event.buf,
+			silent = true,
+			nowait = true,
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "floating-terminal" },
+	callback = function(event)
+		vim.keymap.set("n", "q", function()
+			vim.api.nvim_win_hide(0)
+		end, {
+			buffer = event.buf,
+			silent = true,
+			nowait = true,
+		})
 	end,
 })
