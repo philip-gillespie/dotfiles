f47489f7ffb0e0de2210b3b4094d084bac36f8e5
Philip Gillespie <101209253+philip-gillespie@users.noreply.github.com>
2026-09-02 20:31:09 +0100

    Refactor/completions (#20)
    * remove needless comments
    
    * Add documentation to completions
    
    ---------
    
    Co-authored-by: Philip Gillespie <philip@Philips-Air.Home>

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,84 +1,57 @@
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	-- dependencies = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" },
 	dependencies = {
 		{
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
 			dependencies = { "rafamadriz/friendly-snippets" },
 			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-				-- if you also have your own custom snippets in ~/.config/nvim/snippets/
-				-- require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
 				require("luasnip.loaders.from_lua").lazy_load({
 					paths = vim.fn.stdpath("config") .. "/lua/snippets",
 				})
 			end,
 		},
 	},
-	-- use a release tag to download pre-built binaries
 	version = "1.*",
-	-- AND/OR build from source
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source with:
-	-- build = 'nix run .#build-plugin',
-
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
 		snippets = {
 			preset = "luasnip",
 		},
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		-- keymap = { preset = "default" },
 		keymap = {
-			preset = "none", -- We are building from scratch
-
+			preset = "none",
 			["<CR>"] = { "fallback" }, -- Enter always just does Enter
-
-			-- Confirmation (Your previous <C-e> and <C-CR> logic)
+			-- Confirmation
 			["<C-e>"] = { "accept", "fallback" },
 			["<C-CR>"] = { "accept", "fallback" },
-
-			-- Navigation (Your <C-j> and <C-k> logic)
+			-- Navigation
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
-
-			-- Snippet Jumping (Your <C-n> and <C-p> logic)
+			-- Snippet Jumping
 			["<C-n>"] = { "snippet_forward", "fallback" },
 			["<C-p>"] = { "snippet_backward", "fallback" },
-
-			-- Choice Nodes (Using <C-h> and <C-l> for snippets)
+			-- Choice Nodes
 			["<C-h>"] = { "snippet_backward", "fallback" },
 			["<C-l>"] = { "snippet_forward", "fallback" },
-
-			-- Documentation (Your <C-b> and <C-f> logic)
+			-- Documentation
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-			-- Toggle (Your <C-Space> logic)
+			-- Toggle
 			["<C-Space>"] = { "show", "hide", "fallback" },
 		},
 		appearance = {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 1000,
+                update_delay_ms = 10
+			},
+		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
@@ -91,12 +64,6 @@ return {
 				},
 			},
 		},
-
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
