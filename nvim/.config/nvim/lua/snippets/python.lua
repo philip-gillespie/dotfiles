bbffdfda1d5bf8f26eabcf65f167cf5141a28dfe
Philip Gillespie <philip.gillespie@digital.cabinet-office.gov.uk>
2026-09-03 13:30:41 +0100

    remove unnecessary import

--- nvim/.config/nvim/lua/snippets/python.lua
+++ nvim/.config/nvim/lua/snippets/python.lua
@@ -4,7 +4,6 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
-local events = require("luasnip.util.events")
 local helpers = require("snippets.helpers")
 
 local completions = {}
